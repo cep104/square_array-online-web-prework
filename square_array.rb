08a39dd23d704a3c2655944798dde489037a1c6e
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-19 18:41:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
+# def square_array(array)
+# new_array = []
+#   array.each do |n|
+#   new_array << (n ** 2)
+#   end
+#   new_array
+# end
 def square_array(array)
-new_array = []
-  array.each do |n|
-   new_array << (n ** 2)
-  end
-  new_array
+  array.collect do {|n| n**2}
 end
